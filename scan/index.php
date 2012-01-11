6ba13c4635d1f71c231c219436f1ecc986a314f3
yvestan <yvestan@gmail.com>
2012-01-11 16:49:32 +0100

    premier commit

---
+++ scan/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 ff=unix fenc=utf8: */
+
+/**
+ *
+ * Scan
+ *
+ * @package   HelloEvent
+ * @author    Yves Tannier [grafactory.net]
+ * @copyright 2011 Yves Tannier
+ * @link      http://www.grafactory.net/helloevent/
+ * @license   http://fr.wikipedia.org/wiki/Licence_MIT MIT Licence
+*/
+
+include dirname(__FILE__).'/../app/helloevent_scan.php';
+
+?>
